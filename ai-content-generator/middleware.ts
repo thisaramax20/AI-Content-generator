9cc34a9245c802ad269a16959da195f077013f0c
thisaramax20 <thisarathejana20@gmail.com>
2024-12-09 20:17:55 +0530

    integrated clerk for authentication

--- ai-content-generator/middleware.ts
+++ ai-content-generator/middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/"]);
 
 export const config = {
   matcher: [
